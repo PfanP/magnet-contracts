02c294ef68f01fc17a475fb413aba47e10e6971f
Brent Pryer <brent@pryermachine.com>
2021-11-15 12:51:58 -0600

    added ganache settings
    added ganache settings to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,35 +1,40 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ganache");
-require("@nomiclabs/hardhat-web3");
-require("@nomiclabs/hardhat-ethers");
-
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-ganache')
+require('@nomiclabs/hardhat-web3')
+require('@nomiclabs/hardhat-ethers')
 
 //require("hardhat-prettier");
 
-
 //import SignerWithAddress from "@nomiclabs/hardhat-ethers/signers"
 
-
 module.exports = {
-  defaultNetwork: "localhost",
+  defaultNetwork: 'ganache',
   solidity: {
-    version: "0.7.5",
+    version: '0.7.5',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   networks: {
+    ganache: {
+      url: 'http://127.0.0.1:7545',
+      chainId: 1337,
+      gasLimit: 6000000000,
+      defaultBalanceEther: 10,
+    },
     fuji: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
-      accounts: ["c2116e844ce28b95fce219e716bf7db375ebbf243d6ae241af5c53e442fde0c8"]
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      accounts: [
+        'c2116e844ce28b95fce219e716bf7db375ebbf243d6ae241af5c53e442fde0c8',
+      ],
 
       //ChainID:  43113
-    }
-  }
-  
+    },
+  },
+
   // networks: {
   //   hardhat: {
   //   },
@@ -38,44 +43,33 @@ module.exports = {
   //     accounts: [privateKey1, privateKey2, ...]
   //   }
   // },
+}
 
-};
-
-
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // task action function receives the Hardhat Runtime Environment as second argument
-task("web3", "Prints accounts", async (_, { web3 }) => {
-  console.log(await web3.eth.getAccounts());
+task('web3', 'Prints accounts', async (_, { web3 }) => {
+  console.log(await web3.eth.getAccounts())
+})
 
-  
-});
+task('number', 'Prints the current block number', async (_, { ethers }) => {
+  await ethers.provider.getBlockNumber().then((blockNumber) => {
+    console.log('Current block number: ' + blockNumber)
+  })
+})
 
-task(
-  "number",
-  "Prints the current block number",
-  async (_, { ethers }) => {
-    await ethers.provider.getBlockNumber().then((blockNumber) => {
-      console.log("Current block number: " + blockNumber);
-    });
-  }
-);
-
-
-  
-  
-  // paths: {
-  //   sources: "./contracts",
-  //   tests: "./test",
-  //   cache: "./cache",
-  //   artifacts: "./artifacts"
-  // },
-  // mocha: {
-  //   timeout: 20000
-  // }
+// paths: {
+//   sources: "./contracts",
+//   tests: "./test",
+//   cache: "./cache",
+//   artifacts: "./artifacts"
+// },
+// mocha: {
+//   timeout: 20000
+// }
